a32bfb068a8d6e80db58ecbf2d3e0b531fb8a333
cssilva99 <46228708+cssilva99@users.noreply.github.com>
2024-02-08 14:17:34 +0000

    Update main.tf
    According to chatGPT, the required/mandatory fields are 
    
      name                = var.name
      type                = var.type
      region              = var.region
      zone                = var.zone 
      project             = var.project_id
      permissions
      configuration

--- Automating Infra on GCP using Terraform/main.tf
+++ Automating Infra on GCP using Terraform/main.tf
@@ -1,5 +1,13 @@
-provider "google" {
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
+#############
+# Instances
+#############
+
+resource "google_compute_instance_from_template" "compute_instance" {
+  name                = var.name
+  type                = var.type
+  region              = var.region
+  zone                = var.zone 
+  project             = var.project_id
+  //permissions
+  //configuration
 }
