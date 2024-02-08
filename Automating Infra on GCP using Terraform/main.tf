afc2a3b0d67a5f0404c92d9850bdfda50d5ad956
cssilva99 <46228708+cssilva99@users.noreply.github.com>
2024-02-08 14:18:42 +0000

    Update main.tf
    These 2 resources should not be placed here

--- Automating Infra on GCP using Terraform/main.tf
+++ Automating Infra on GCP using Terraform/main.tf
@@ -2,7 +2,17 @@
 # Instances
 #############
 
-resource "google_compute_instance_from_template" "compute_instance" {
+resource "google_compute_instance_from_template" "tf-instance-1" {
+  name                = var.name
+  type                = var.type
+  region              = var.region
+  zone                = var.zone 
+  project             = var.project_id
+  //permissions
+  //configuration
+}
+
+resource "google_compute_instance_from_template" "tf-instance-2" {
   name                = var.name
   type                = var.type
   region              = var.region
