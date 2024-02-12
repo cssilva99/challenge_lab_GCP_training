ca04b3dcfbb397f5d613a61cc6a9cd3b3eb345ce
cssilva99 <46228708+cssilva99@users.noreply.github.com>
2024-02-12 11:32:36 +0000

    Update main.tf
    Project Updated inside the Provider Block

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@
   # ChatGPT says that 2 Terraform Modules within the same file cannot have the same name
   # IT IS NOT NECESSARY TO INDICATE the instance.tf file in the source. Just the folder is enough
 ######################################################################################################
+provider "google" {
+  project     = "solar-theory-245311"
+  region      = var.region
+  zone        = var.zone
+}
 
 module "compute_instance_1" {
   source  = "./modules/instances/"
